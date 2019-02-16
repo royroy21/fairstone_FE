3dc0751c26987da687e4e7a2805cf05b19199c19
Roy Hanley <royhanley8@gmail.com>
2019-02-16 16:21:28 +0000

    got a rough grid semi working

--- src/components/organisms/Game/index.js
+++ src/components/organisms/Game/index.js
@@ -1,9 +1,11 @@
 import {cloneDeep} from 'lodash';
 import React, { Component } from 'react';
-import DragSourceItem from './../../atoms/DragSourceItem';
-import DragTargetItem from '../../atoms/DragTargetItem';
 import {DragDropContext} from "react-dnd";
 import HTML5Backend from "react-dnd-html5-backend";
+
+import DragSourceItem from './../../../components/atoms/DragSourceItem';
+import DragTargetItem from './../../../components/atoms/DragTargetItem';
+import PlayerInGameStats from './../../../components/molecules/PlayerInGameStats';
 
 class Game extends Component {
 
@@ -11,10 +13,18 @@ class Game extends Component {
     items: [
       {id: 1, name: 'Item 1'},
       {id: 2, name: 'Item 2'},
+      {id: 3, name: 'Item 3'},
+      {id: 4, name: 'Item 4'},
+      {id: 5, name: 'Item 5'},
+      {id: 6, name: 'Item 6'},
     ],
     targets: [
       {id: 1, name: null},
       {id: 2, name: null},
+      {id: 3, name: null},
+      {id: 4, name: null},
+      {id: 5, name: null},
+      {id: 6, name: null},
     ]
   };
 
@@ -47,8 +57,11 @@ class Game extends Component {
 
   render() {
     return (
-      <div className="App">
-        <div className="item-container">
+      <div className="game-container">
+        {/*<div className="player-in-game-stats-container">*/}
+          {/*<PlayerInGameStats />*/}
+        {/*</div>*/}
+        <div className="drag-source-item-container source-item-container">
           {this.state.items.map(item => (
             <DragSourceItem
               key={item.id}
@@ -58,7 +71,7 @@ class Game extends Component {
             />
             ))}
         </div>
-        <div className="target-container">
+        <div className="drag-target-item-container source-item-container">
           {this.state.targets.map(target => (
             <DragTargetItem
               key={target.id}
@@ -67,11 +80,29 @@ class Game extends Component {
             />
             ))}
         </div>
-        <div>
-          <button style={this.buttonStyle} onClick={this.resetState}>
-            RESET
-          </button>
+        <div className="drag-target-item-container source-item-container">
+          {this.state.targets.map(target => (
+            <DragTargetItem
+              key={target.id}
+              id={target.id}
+              name={target.name}
+            />
+            ))}
         </div>
+        <div className="drag-source-item-container source-item-container">
+          {this.state.items.map(item => (
+            <DragSourceItem
+              key={item.id}
+              id={item.id}
+              item={item}
+              handleDrop={(targetId, name) => this.dropItem(targetId, name)}
+            />
+            ))}
+        </div>
+        {/*<div className="player-in-game-stats-container">*/}
+          {/*<PlayerInGameStats/>*/}
+        {/*</div>*/}
+        {/*<button style={this.buttonStyle} onClick={this.resetState}>RESET</button>*/}
       </div>
     );
   }
